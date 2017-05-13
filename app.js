dc2b7de75023867137e73885bfc9314daf63e9ab
Jason Crawford <daxapps777@gmail.com>
2017-05-13 13:45:15 -0500

    Display word count and unique word count

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-// Word Count
-// split word using regex
-// put words in an array
-// array.length
-function wordCount() {
-	$('button[type="submit"]').click( function() {
-		$('.hidden').removeClass
-    var numOfWords = $('#user-text').val().replace(/^[\s,.;]+/, "").replace(/[\s,.;]+$/, "").split(/[\s,.;]+/);
-    alert(numOfWords.length);
+
+function textAnalyzer() {
+	$('button[type="submit"]').click( function(event) {
+		event.preventDefault();
+    wordsArray = $('#user-text').val().replace(/^[\s,.;]+/, "").replace(/[\s,.;]+$/, "").split(/[\s,.;]+/);
+
+    
+    // wordsArray.forEach(function(word) {
+    // 	var wordLength = [];
+    // 	wordLength.append(word.length)
+    // 	alert(wordLength)
+    // })
+
+    // total wowrd count
+    // alert(wordsArray.length);
+
+    // Unique word count
+   	// alert(jQuery.unique(wordsArray).length);
+
+	// alert(wordsArray.forEach(function(word){
+	// 	// var wordLength = word.length;
+	// 	var wordLengthTotal = 0;		
+	// 	wordLengthTotal += word.length;
+	// 	return wordLengthTotal;
+	// 	 })
+	// );
+ 	// alert(wordLengthTotal);
+ 	// alert(wordsArray.reduce());
+
+ 		$(".js-word-count").text(wordsArray.length);
+		$(".js-unique").text(jQuery.unique(wordsArray).length);
+		// $(".js-avg-word-length").text(avgWordLength);
+		$("dl").removeClass("hidden");
 	});
 }
 
-$(function() {
-	wordCount();
-})
+// function avgWordLength () {
+// 	$('button[type="submit"]').click( function() {
+ 	
+//  });
+// }
+
+$(textAnalyzer);
